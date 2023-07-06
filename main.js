4b0fbd4ec4efe377e77165139ca6cfa631219566
MSkopych <87950220+MSkopych@users.noreply.github.com>
2023-07-06 17:15:18 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const startBtn = document.querySelector('.start')
 const screens = document.querySelectorAll('.screen');
 const frontPag = document.querySelector('.frontP')
 const timeList = document.querySelector('#time-list')
-
+const timeEl = document.querySelector('#time')
+let time = 0;
 
 
 startBtn.addEventListener('click', (event) => {
@@ -17,6 +18,38 @@ frontPag.addEventListener('click', (event) => {
 
 })
 
-timeList.addEventListener('click', (event) => {
-    event.
+timeList.addEventListener('click', (event) =>
+{
+ if (event.target.classList.contains('time-btn')) {
+    time = parseInt(event.target.getAttribute('data-time'));
+    screens[1].classList.add('up');
+    startGame();
+ }
 })
+
+
+function startGame(){
+    setInterval(decreaseTime, 1000)
+    setTime(time);
+}
+
+function decreaseTime(){
+    if(time ==0){
+        gameOver()
+    }
+    else{
+    correctly = --time;
+    if(correctly < 10){
+        correctly = `0${correctly}`;
+    }
+
+    setTime(correctly);
+    }
+}
+function gameOver(){
+    window.location.reload();
+}
+
+function setTime(value){
+    timeEl.innerHTML = `00:${value}`;
+}
